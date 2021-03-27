2acec8ee6565d0b264e7e687f8c279cd28ae771d
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-03-27 10:00:44 +0530

    second commit

--- DynamicKey/AgoraDynamicKey/nodejs/sample/RtmTokenBuilderSample.js
+++ DynamicKey/AgoraDynamicKey/nodejs/sample/RtmTokenBuilderSample.js
@@ -1,9 +1,9 @@
 const RtmTokenBuilder = require('../src/RtmTokenBuilder').RtmTokenBuilder;
 const RtmRole = require('../src/RtmTokenBuilder').Role;
 const Priviledges = require('../src/AccessToken').priviledges;
-const appID  = "970CA35de60c44645bbae8a215061b33";
-const appCertificate = "5CFd2fd1755d40ecb72977518be15d3b";
-const account = "test_user_id";
+const appID  = "b1a776384fe24b58a43030c834b8f7dd";
+const appCertificate = "5b4b7878c61740dcb5a35ae72f0fefdd";
+const account = "1234";
 
 const expirationTimeInSeconds = 3600
 const currentTimestamp = Math.floor(Date.now() / 1000)
--- DynamicKey/AgoraDynamicKey/python3/sample/RtmTokenBuilderSample.py
+++ DynamicKey/AgoraDynamicKey/python3/sample/RtmTokenBuilderSample.py
@@ -7,9 +7,9 @@ import time
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.RtmTokenBuilder import RtmTokenBuilder,Role_Rtm_User
 
-appID = "970CA35de60c44645bbae8a215061b33"
-appCertificate = "5CFd2fd1755d40ecb72977518be15d3b"
-user = "test_user_id"
+appID = "b1a776384fe24b58a43030c834b8f7dd"
+appCertificate = "5b4b7878c61740dcb5a35ae72f0fefdd"
+user = "1234"
 expirationTimeInSeconds = 3600
 currentTimestamp = int(time.time())
 privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
