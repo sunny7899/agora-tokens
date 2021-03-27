924863ae7de851c281cd862353d00891ffa5df9f
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-03-27 12:11:06 +0530

    nodejs token generation script updated

--- DemoServer.js
+++ DemoServer.js
@@ -20,46 +20,60 @@ app.set('port', PORT);
 // app.use(express.favicon());
 app.use(app.router);
 
-var generateRtcToken = function(req, resp) {
-    var currentTimestamp = Math.floor(Date.now() / 1000)
-    var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
-    var channelName = req.query.channelName;
-    // use 0 if uid is not specified
-    var uid = req.query.uid || 0
-    if (!channelName) {
-        return resp.status(400).json({ 'error': 'channel name is required' }).send();
-    }
+
+http.createServer(function (request, response) {
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' });   // Send the HTTP header, HTTP Status: 200 : OK, Content Type: text/plain
+
+    console.log('got http://127.0.0.1:8081/');
+
+    response.write('hi');
+
+    response.end('Hello World\n');  // Send the response body as "Hello World"
+
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
+// var generateRtcToken = function(req, resp) {
+//     var currentTimestamp = Math.floor(Date.now() / 1000)
+//     var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+//     var channelName = req.query.channelName;
+//     // use 0 if uid is not specified
+//     var uid = req.query.uid || 0
+//     if (!channelName) {
+//         return resp.status(400).json({ 'error': 'channel name is required' }).send();
+//     }
 
 
-    var key = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
+//     var key = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
 
-    resp.header("Access-Control-Allow-Origin", "*")
-        //resp.header("Access-Control-Allow-Origin", "http://ip:port")
-    return resp.json({ 'key': key }).send();
-};
+//     resp.header("Access-Control-Allow-Origin", "*")
+//         //resp.header("Access-Control-Allow-Origin", "http://ip:port")
+//     return resp.json({ 'key': key }).send();
+// };
 
-var generateRtmToken = function(req, resp) {
-    var currentTimestamp = Math.floor(Date.now() / 1000)
-    var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
-    var account = req.query.account;
-    if (!account) {
-        return resp.status(400).json({ 'error': 'account is required' }).send();
-    }
+// var generateRtmToken = function(req, resp) {
+//     var currentTimestamp = Math.floor(Date.now() / 1000)
+//     var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+//     var account = req.query.account;
+//     if (!account) {
+//         return resp.status(400).json({ 'error': 'account is required' }).send();
+//     }
 
-    var key = RtmTokenBuilder.buildToken(appID, appCertificate, account, RtmRole, privilegeExpiredTs);
+//     var key = RtmTokenBuilder.buildToken(appID, appCertificate, account, RtmRole, privilegeExpiredTs);
 
-    resp.header("Access-Control-Allow-Origin", "*")
-        //resp.header("Access-Control-Allow-Origin", "http://ip:port")
-    return resp.json({ 'key': key }).send();
-};
+//     resp.header("Access-Control-Allow-Origin", "*")
+//         //resp.header("Access-Control-Allow-Origin", "http://ip:port")
+//     return resp.json({ 'key': key }).send();
+// };
 
-app.get('/rtcToken', generateRtcToken);
-app.get('/rtmToken', generateRtmToken);
+// app.get('/rtcToken', generateRtcToken);
+// app.get('/rtmToken', generateRtmToken);
 
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('AgoraSignServer starts at ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function() {
+//     console.log('AgoraSignServer starts at ' + app.get('port'));
+// });
 
 //https.createServer(credentials, app).listen(app.get('port') + 1, function() {
 //    console.log('AgoraSignServer starts at ' + (app.get('port') + 1));
